43c36dff73b25e5e9a87b37ae8607e5909f8dd3f
Yasuyuki Takeo <yasuflatland@gmail.com>
2024-06-29 17:24:14 +0900

    Add CI
    Initial Page Transition
    
    Add TailwindCSS
    
    Create frontend with Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,7 @@
-/// <reference types="vitest" />
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'happy-dom',
-        setupFiles: './vitest.setup.ts'
-    },
+  plugins: [react()],
 })
