16748801745530062b58f65f71f8915c80efec28
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-24 21:33:13 +0900

    add graceful shutdown with slog and http.Server timeouts to backend

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
+	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
+	"golang.org/x/sync/errgroup"
 )
+
+const defaultShutdownTimeout = 25 * time.Second
 
 func newRouter() *echo.Echo {
 	e := echo.New()
@@ -29,14 +37,80 @@ func newRouter() *echo.Echo {
 	return e
 }
 
-func main() {
+func shutdownTimeout(logger *slog.Logger) time.Duration {
+	v := os.Getenv("SHUTDOWN_TIMEOUT")
+	if v == "" {
+		return defaultShutdownTimeout
+	}
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		logger.Warn("invalid SHUTDOWN_TIMEOUT, using default",
+			"value", v, "err", err, "default", defaultShutdownTimeout.String())
+		return defaultShutdownTimeout
+	}
+	if d <= 0 {
+		logger.Warn("non-positive SHUTDOWN_TIMEOUT, using default",
+			"value", v, "default", defaultShutdownTimeout.String())
+		return defaultShutdownTimeout
+	}
+	return d
+}
+
+func run(ctx context.Context, logger *slog.Logger) error {
 	e := newRouter()
+	e.Logger = logger
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "1323"
 	}
-	if err := e.Start(":" + port); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           e,
+		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       120 * time.Second,
+	}
+
+	g, gctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		logger.Info("server starting", "addr", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		<-gctx.Done()
+		timeout := shutdownTimeout(logger)
+		logger.Info("shutdown signal received", "timeout", timeout.String())
+		sctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		if err := srv.Shutdown(sctx); err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				logger.Warn("graceful shutdown timed out, forcing close", "timeout", timeout.String())
+				return nil
+			}
+			return err
+		}
+		return nil
+	})
+
+	return g.Wait()
+}
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+	slog.SetDefault(logger)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	if err := run(ctx, logger); err != nil {
+		logger.Error("server terminated", "err", err)
+		os.Exit(1)
 	}
 }
